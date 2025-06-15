fb9ad340a9480b6dd781fc9bef1f2c24bf7ef1bd
Aphatheology <aphatheology@gmail.com>
2025-06-15 18:47:41 +0100

    add kafka types and missing dependencies

---
+++ index.ts
@@ -0,0 +1,5 @@
+import * as Products from './types/proto/products';
+import * as Orders from './types/proto/orders';
+
+export { Products, Orders };
+export * from './kafka/topics';
---
+++ kafka/topics.ts
@@ -0,0 +1,8 @@
+export const KAFKA_SERVICE_NAME = 'KAFKA_SERVICE';
+
+export const KAFKA_TOPICS = {
+  ORDER_CREATED: 'order-created',
+  PRODUCT_CREATED: 'product-created',
+  USER_REGISTERED: 'user-registered',
+  PAYMENT_PROCESSED: 'payment-processed',
+};
